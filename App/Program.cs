14660ec51e84b9e30aa823352ce32d30120bd3ff
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-14 10:06:07 +0100

    create solution and app project

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch.wuerth.tobias.mux.App
+{
+    internal static class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
