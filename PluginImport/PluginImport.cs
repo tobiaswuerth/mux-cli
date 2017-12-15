9f129dac5fe8672d3b3311792f92a62e1c20878d
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-15 19:01:47 +0100

    updated references to use nuget instead of submodules

---
+++ PluginImport/PluginImport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ch.wuerth.tobias.mux.Core.plugin;
+using ch.wuerth.tobias.mux.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ch.wuerth.tobias.mux.plugins.PluginImport
+{
+    public class PluginImport : PluginBase
+    {
+        protected override void ConfigurePlugin(PluginConfigurator configurator)
+        {
+            configurator.RegisterName("import");
+        }
+
+        protected override void Process(params String[] args)
+        {
+            // every arg should be a directory path
+            
+
+        }
+    }
+}
