f7c33792bd31501ba94a8f5754a29cdbbcfb5bf2
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-05 14:08:51 +0100

    improved cli argument handling

--- App/Program.cs
+++ App/Program.cs
@@ -8,54 +8,90 @@ using ch.wuerth.tobias.mux.Core.logging.information;
 using ch.wuerth.tobias.mux.Core.plugin;
 using ch.wuerth.tobias.mux.plugins.PluginChromaprint;
 using ch.wuerth.tobias.mux.plugins.PluginImport;
+using clipr;
 using global::ch.wuerth.tobias.mux.Core.global;
 
 namespace ch.wuerth.tobias.mux.App
 {
     public class Program
     {
-        protected Program(String[] args)
+        private static readonly LoggerBundle Logger = new LoggerBundle
         {
-            CreateGlobalDirectories();
-            LoggerBundle logger = PrepareLogger(args);
+            Information = new InformationConsoleLogger(null),
+            Exception = new ExceptionConsoleLogger(null)
+        };
+
+        private Program(String[] args)
+        {
+            (Boolean success, ProgramConfig config) pa = ProcessProgramArguments(args, Logger);
+            if (!pa.success)
+            {
+                return;
+            }
 
             try
             {
-                // validate call
-                if (args.Length < 1)
-                {
-                    logger.Information.Log("Usage: app <logging destination> <plugin name> [<arg1> <arg2> ...]");
-                    logger.Information.Log("Logging destinations: console, file");
-                    return;
-                }
+                CreateGlobalDirectories();
 
-                List<PluginBase> pluginsToLoad = LoadPlugins(logger);
-                Dictionary<String, PluginBase> plugins = InitializePlugin(pluginsToLoad, logger);
+                List<PluginBase> pluginsToLoad = LoadPlugins(Logger);
+                Dictionary<String, PluginBase> plugins = InitializePlugin(pluginsToLoad, Logger);
 
-                String pluginName = args[0].ToLower();
-                if (!plugins.ContainsKey(pluginName))
+                if (!plugins.ContainsKey(pa.config.PluginName))
                 {
-                    logger.Information.Log($"No active plugin with name '{pluginName}' found");
-                    logger.Information.Log("The following plugin names were registered on startup: ");
+                    Logger.Information.Log($"No active plugin with name '{pa.config.PluginName}' found");
+                    Logger.Information.Log("The following plugin names were registered on startup: ");
                     foreach (String key in plugins.Keys)
                     {
-                        logger.Information.Log($" - {key}");
+                        Logger.Information.Log($" - {key}");
                     }
 
                     return;
                 }
 
-                logger.Information.Log("Executing plugin...");
-                plugins[pluginName].Work(args.Skip(1).ToArray() /* skip command name */);
-                logger.Information.Log("Execution finished.");
+                Logger.Information.Log("Executing plugin...");
+                plugins[pa.config.PluginName].Work(pa.config.Args.ToArray());
+                Logger.Information.Log("Execution finished.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception.Log(ex);
+            }
+        }
+
+        private static (Boolean success, ProgramConfig config) ProcessProgramArguments(String[] args,
+            LoggerBundle logger)
+        {
+            ProgramConfig config;
+            try
+            {
+                config = CliParser.Parse<ProgramConfig>(args);
             }
             catch (Exception ex)
             {
                 logger.Exception.Log(ex);
+                logger.Information.Log("Usage: app [options...] <plugin name> [arguments...]");
+                logger.Information.Log("Options:");
+                logger.Information.Log("-i | --log-information\t\t console, file");
+                logger.Information.Log("-e | --log-exception\t\t console, file");
+                logger.Information.Log("sample:\t app -i console --log-exception file import 'C:\\User\\Bob\\Music' 'C:\\User\\Foo\\Music'");
+                return (false, null);
             }
+
+            // additional adjustments based on successful argument parsing
+            Int32 fileLoggerId = new ProgramConfig.LogDestination.FileDestination().Id;
+            if (config.LogInformation.Id.Equals(fileLoggerId))
+            {
+                logger.Information = new InformationFileLogger(null);
+            }
+            if (config.LogException.Id.Equals(fileLoggerId))
+            {
+                logger.Exception = new ExceptionFileLogger(null);
+            }
+
+            return (true, config);
         }
 
-        private void CreateGlobalDirectories()
+        private static void CreateGlobalDirectories()
         {
             List<String> paths = new List<String>
             {
@@ -64,7 +100,7 @@ namespace ch.wuerth.tobias.mux.App
                 Location.LogsDirectoryPath
             };
 
-            paths.Where(x => !Directory.Exists(x)).ToList().ForEach(x => { Directory.CreateDirectory(x); });
+            paths.Where(x => !Directory.Exists(x)).ToList().ForEach(x => Directory.CreateDirectory(x));
         }
 
         private static Dictionary<String, PluginBase> InitializePlugin(List<PluginBase> pluginsToLoad,
