117c66b482f3d694d82800562b388c0b0e79010a
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-10 20:49:22 +0100

    added basic structure

---
+++ plugins/PluginMusicBrainz/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch.wuerth.tobias.mux.plugins.PluginMusicBrainz
+{
+    public class Config
+    {
+        public Int32 BatchSize { get; set; }
+    }
+}
---
+++ plugins/PluginMusicBrainz/PluginMusicBrainz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ch.wuerth.tobias.mux.Core.logging;
+using ch.wuerth.tobias.mux.Core.plugin;
+
+namespace ch.wuerth.tobias.mux.plugins.PluginMusicBrainz
+{
+    public class PluginMusicBrainz : PluginBase
+    {
+        private Config _config;
+
+        public PluginMusicBrainz(LoggerBundle logger) : base("musicbrainz", logger) { }
+
+        protected override void OnInitialize()
+        {
+            _config = RequestConfig<Config>();
+        }
+
+        protected override void Process(String[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
