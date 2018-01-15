075673a3073863d6fed9621b6ecc797600e8da66
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-15 20:42:15 +0100

    added nuget dependencies, created plugin base

---
+++ plugins/PluginUserMgmt/PluginUserMgmt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ch.wuerth.tobias.mux.Core.logging;
+using ch.wuerth.tobias.mux.Core.plugin;
+
+namespace ch.wuerth.tobias.mux.plugins.PluginUserMgmt
+{
+    public class PluginUserMgmt : PluginBase
+    {
+        public PluginUserMgmt(String pluginName, LoggerBundle logger) : base(pluginName, logger) { }
+
+        protected override void Process(String[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
