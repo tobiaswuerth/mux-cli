b186729942e78daee8dadd79e79894b1481884fd
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-24 07:10:56 +0100

    add more base constructors to exception

--- plugins/PluginMusicBrainz/exceptions/MusicBrainzApiException.cs
+++ plugins/PluginMusicBrainz/exceptions/MusicBrainzApiException.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace ch.wuerth.tobias.mux.plugins.PluginMusicBrainz.exceptions
 {
     public class MusicBrainzApiException : Exception
     {
+        public MusicBrainzApiException() { }
+
         public MusicBrainzApiException(String message) : base(message) { }
+
+        public MusicBrainzApiException(String message, Exception innerException) : base(message, innerException) { }
+
+        protected MusicBrainzApiException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
