2ab2e60437319e566cbfbc020e073ade563216d9
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-24 06:53:09 +0100

    add more base constructors to exception

--- plugins/PluginChromaprint/exceptions/CalculationException.cs
+++ plugins/PluginChromaprint/exceptions/CalculationException.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace ch.wuerth.tobias.mux.plugins.PluginChromaprint.exceptions
 {
     public class CalculationException : Exception
-
     {
+        public CalculationException() { }
+
         public CalculationException(String message) : base(message) { }
+
+        public CalculationException(String message, Exception innerException) : base(message, innerException) { }
+
+        protected CalculationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
