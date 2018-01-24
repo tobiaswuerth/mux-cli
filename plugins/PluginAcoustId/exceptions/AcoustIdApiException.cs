8f3e98835d1fe47088411ccf4e364820a712a760
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-24 06:47:53 +0100

    add more base constructors to exception type

--- plugins/PluginAcoustId/exceptions/AcoustIdApiException.cs
+++ plugins/PluginAcoustId/exceptions/AcoustIdApiException.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace ch.wuerth.tobias.mux.plugins.PluginAcoustId.exceptions
 {
     public class AcoustIdApiException : Exception
     {
+        public AcoustIdApiException() { }
+
         public AcoustIdApiException(String message) : base(message) { }
+
+        public AcoustIdApiException(String message, Exception innerException) : base(message, innerException) { }
+
+        protected AcoustIdApiException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
